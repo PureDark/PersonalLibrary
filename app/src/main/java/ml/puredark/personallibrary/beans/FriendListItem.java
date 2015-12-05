3f6a985f2123670ea389eac3ae8198b6f5fcaed4
KevinZhan <kevinzhan0417@outlook.com>
2015-12-05 15:22:48 +0800

    添加将汉字转为字母的库，并在FriendListItem中获取每个名字的首字母

--- app/src/main/java/ml/puredark/personallibrary/beans/FriendListItem.java
+++ app/src/main/java/ml/puredark/personallibrary/beans/FriendListItem.java
@@ -1,14 +1,18 @@
 package ml.puredark.personallibrary.beans;
 
+import android.util.Log;
+
 import java.util.Map;
 
 import ml.puredark.personallibrary.dataprovider.AbstractDataProvider;
+import  net.sourceforge.pinyin4j.PinyinHelper;
 
 public final class FriendListItem  extends AbstractDataProvider.Data{
     public int id;
     public int sex;
     public String avatar, nickName,signature;
     public String birthday;
+    public String charactor;
     public FriendListItem(int id, int sex, String avatar, String nickName, String signature, String birthday){
         this.id = id;
         this.sex = sex;
@@ -16,6 +20,11 @@ public final class FriendListItem  extends AbstractDataProvider.Data{
         this.nickName = nickName;
         this.signature = signature;
         this.birthday = birthday;
+        String[] temp = PinyinHelper.toHanyuPinyinStringArray(nickName.toCharArray()[0]);
+        charactor = new String(temp[0]);
+        charactor = charactor.substring(0,1);
+        charactor = charactor.toUpperCase();
+        Log.i("Friend",charactor);
     }
     public int getId(){
         return id;
